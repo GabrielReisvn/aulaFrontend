d431b4fcee936324bfc50671be3246cf2972e583
biel <gabrielreisviana@gmail.com>
2026-02-10 14:28:15 -0300

    aula 1 e 2 javascrpt

---
+++ objetos.js
@@ -0,0 +1,76 @@
+let User = {
+    login: "admin",
+    senha: "123456",
+    tipo: "administrador"
+};
+console.log(`o login do ${User.tipo} é ${User.login} e a senha é ${User.senha} , muito fraca por sinal!`);
+
+//2 
+let aluno = {
+    nome: "Maria",
+    curso: "Tecnico em desenvolvimento de sistemas",
+    turma: "DSI 3 SESI" 
+};
+console.log(`O aluno ${aluno.nome} está no curso ${aluno.curso} e na turma ${aluno.turma}`)
+
+//3
+let professor = {
+    nome: "Eduardo",
+    disciplina: "Javascript",
+    turno: "Manhã"
+};
+console.log(`O professor ${professor.nome} ministra a disciplina ${professor.disciplina} no turno da ${professor.turno}`)
+
+//4
+let empresa = { 
+    nome: "Tech Solutions",
+    area: "Tecnologia",
+    cidade: "Fortaleza"
+};
+console.log(`A empresa ${empresa.nome} atua na aréa de ${empresa.area} e fics em ${empresa.cidade}.`)
+
+let computador = {
+    marca: "Dell",
+    processador: "Ryzen 9",
+    memoria: "16GB",
+
+};
+console.log(`O computador da marca ${computador.marca} possui um processador ${computador.processador} e ${computador.memoria} de memória.`)
+
+let livro = {
+    titulo:"Javascript Basico",
+    autor: "João Silva",
+    paginas: 250
+};
+console.log(`O livro ${livro.titulo} foi escrito por ${livro.autor} e possui ${livro.paginas} páginas.`)
+
+let filme = {
+    titulo: "Matrix",  
+    genero: "Ficção",
+    anoP: 1999
+};
+console.log(`O filme ${filme.titulo} é do gênero ${filme.genero} e foi lançado em ${filme.anoP}.`)
+
+
+let jogo = {
+    nome: "pokemon fire red",
+    genero: "RPG",
+    plataforma:"Nintendo"
+};
+console.log(`O jogo ${jogo.nome} é do gênero ${jogo.genero} e roda na plataforma ${jogo.plataforma}.`)
+
+
+let conta ={
+    usuario: "Gabriel",
+    senha: "123456",
+    nivel: "padrão"
+};
+console.log(`O usuário ${conta.usuario} e é do nível ${conta.nivel} tem a senha ${conta.senha}, muito fraca por sinal!`);
+
+
+let escola = {
+    nome: "SENAI",
+    cidade:"Guarulhos",
+    turno: "Manhã"
+};
+console.log(`A escola ${escola.nome} funciona em ${escola.cidade} no turno da ${escola.turno}.`)
