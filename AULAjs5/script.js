f82ff6c19525458c9bd83269f5b4f536971aa630
biel <gabrielreisviana@gmail.com>
2026-03-02 14:53:50 -0300

    intervalo tarde

---
+++ AULAjs5/script.js
@@ -0,0 +1,17 @@
+//============================================
+// Jogo mario
+// Criação das variaveis
+let mario = document.querySelector(`.mario`);//encontrar mario
+let cano = document.querySelector(`.cano`);//encontrar cano
+let nuvem = document.querySelector(`.nuvem`);//encontrar nuvem
+let telaFim = document.querySelector(`.fim`);//encontra tela gaem over
+let botaoReiniciar = document.querySelector(`.reiniciar`);//encontra o botao
+
+
+console.log(`======Parada 01=========`);
+console.log(mario,`mario`);
+console.log(cano,`cano`);
+console.log(nuvem,`nuvem`);
+console.log(telaFim,`tela fim`);
+console.log(botaoReiniciar,`botãoreiniciar`);
+
