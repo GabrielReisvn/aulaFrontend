17e5b6bf9ae4c124300e99bef26e060f7a4e03c7
biel <gabrielreisviana@gmail.com>
2026-03-03 10:48:32 -0300

    contador e reinicio feito

--- AULA-Mario/script.js
+++ AULA-Mario/script.js
@@ -19,6 +19,14 @@ console.log(`botãoreiniciar`,botaoReiniciar);
 function pular(){
     //`pular` predefinido no css, chamada pelo javascript
     mario.classList.add(`pular`);
+    
+    
+    //CONTADOR
+    contador++;
+    const spanPulos = document.querySelector('#pulos');
+    if (spanPulos) spanPulos.innerText = contador;
+    
+    
     setTimeout(function(){
         //voltando ao normal após pular
         mario.classList.remove(`pular`)
@@ -51,41 +59,80 @@ let loopJogo = setInterval(function(){
         console.log('========Colisão detectada!=======');
         console.log(`Cano na posição ${posicaoCano}|| Mario na posição ${posicaoMario}.`);
         console.log(`Fim de jogo`);
+        
+        
+        //contador reset
         contador = 0;
+        const spanPulos = document.querySelector('#pulos');
+        if (spanPulos) spanPulos.innerText = contador;
 
         //======= Tela fim =======
         cano.style.animation = 'none';
         cano.style.left = posicaoCano +'px';
         
         mario.style.animation = 'none';
-        mario.style.left = posicaoCano +'px';
+        mario.style.bottom = posicaoMario +'px';
         mario.src ='./img/game-over.png';
         mario.style.width = '80px';
 
         telaFim.style.visibility = 'visible';
         
-
-        
-        
-        
-        
         //debug console
         clearInterval(loopJogo);
 
         }
-        //contador
-        else{
-            contador++;
-            document.body.jogo.h2.innerHTML(contador);
+       
 
-
-        }
 
 },10)
 
 //====== Função Reiniciar =======
-botaoReiniciar.addEventListener('click',function(){
-    console.log('=== Botão Reiniciar clicado ===');
-    loopJogo();
+function reiniciarJogo(){
+    console.log('===== Reiniciando Jogo =======');
+    //TELA GAME OVER
+    telaFim.style.visibility = 'hidden';
+
+    //CANO
+    cano.style.animation = 'mexerCano 1.2s infinite linear';
+    cano.style.left = '';
+
+    // MARIO
+    mario.style.animation = ''; //Remove qualquer animação fixa
+    mario.style.bottom = '0px';
+    mario.src ='./img/mario.gif';
+    mario.style.width = '130px';
+
+    //=======================
+    //===== NOVO JOGO =======
     
-})
+    loopJogo = setInterval(function(){
+        let posicaoCano = cano.offsetLeft;
+        let posicaoMario = +window.getComputedStyle(mario).bottom.replace(`px`,''); 
+        if (posicaoCano <= 100 && posicaoCano > 0 && posicaoMario < 60){  
+            console.log('========Colisão detectada!=======');
+            console.log(`Cano na posição ${posicaoCano}|| Mario na posição ${posicaoMario}.`);
+            console.log(`Fim de jogo`);
+            
+            //contador reset
+            contador = 0;
+            const spanPulos = document.querySelector('#pulos');
+            if (spanPulos) spanPulos.innerText = contador;
+
+                //======= Tela fim =======
+                cano.style.animation = 'none';
+                cano.style.left = posicaoCano +'px';
+        
+                mario.style.animation = 'none';
+                mario.style.bottom = posicaoMario +'px';
+                mario.src ='./img/game-over.png';
+                mario.style.width = '80px';
+
+                telaFim.style.visibility = 'visible';
+        
+             
+                //debug console
+                clearInterval(loopJogo);
+
+        }
+    },10)
+}
