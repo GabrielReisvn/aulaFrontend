41fb2959f92d98295b1699b28753688459276848
GabrielReisvn <gabrielreisviana@gmail.com>
2026-03-09 14:15:35 -0300

    locurada total

--- AULA-06/3-exercicio-API2.js
+++ AULA-06/3-exercicio-API2.js
@@ -32,6 +32,6 @@ async function Func2API() {
             
         }
     } 
-    console.log(totalRacas `Raças com sub-raças`);
+    console.log(totalRacas, ` Raças com sub-raças`);
 }
 Func2API();
