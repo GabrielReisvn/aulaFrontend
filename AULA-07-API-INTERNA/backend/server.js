1d3c42c6b19752d1823baaefeb25890dc1187b69
biel <gabrielreisviana@gmail.com>
2026-03-10 12:00:33 -0300

    saving

--- AULA-07-API-INTERNA/backend/server.js
+++ AULA-07-API-INTERNA/backend/server.js
@@ -1,24 +1,11 @@
-// importar modulo HTTP nativo do node.js
-// esse nodulo permite criar servidores web
+// ===================================
+// API INTERNA - BACKEND
+// ===================================
+// feita com Node.js e Express
 
-const http = require("http");
-const { log } = require("console");
+// as fotos não são mais baixadas da internet, mas sim armazenadas localmente
 
-//Servidor
-//FUNÇAO SERA EXECUTADA TODA VEZ QUE ACESSAREM SERVIDO
+// data/fotos/
+//=====================================
 
-const server = http.createServer((req, res) => {
-    // definir status res
-    res.writeHead(200, {'content-type': 'text/html'});
 
-    // enviar resposta ao navegador
-
-    res.end(`
-        <h1> API CACHORROS</h1>
-        <p>Servidor funcionando</p>
-    `);
-});
-
-server.listen(3000, () => {
-    console.log('servidor rodando em http://localhost:3000');
-});
