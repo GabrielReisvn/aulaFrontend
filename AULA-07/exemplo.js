f6d708e7c7f106da3c7da4b56535c10f73d5f962
biel <gabrielreisviana@gmail.com>
2026-03-10 10:52:27 -0300

    salvando

---
+++ AULA-07/exemplo.js
@@ -0,0 +1,11 @@
+// JAVASCRIP
+//É uma linguagem dinamica, ou seja, você não precisa declarar o tipo da variável, o JS entende o tipo de dado a partir do valor atribuido a ela.
+
+let idade = 25; // tipo number
+idade = "vinte e cinco"; // tipo string
+
+//Typexcript
+//É uma linguagem criada pela MS que adiciona tipagem ao Javascript
+
+let idadeTS: number = 25; // tipo number
+idadeTS = "vinte e cinco"; // erro
